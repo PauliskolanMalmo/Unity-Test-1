13d0eb005b909cd5d8a1b63fb82a228d79afe2df
Paul B <paul.belfrage@gmail.com>
2020-03-25 13:40:46 +0100

    First commit
    First commit

---
+++ Assets/Script/MoveCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    [SerializeField] float speed;
+    void Start()
+    {
+        //test
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(hInput, 0, 0) * Time.deltaTime * speed);
+    }
+}
